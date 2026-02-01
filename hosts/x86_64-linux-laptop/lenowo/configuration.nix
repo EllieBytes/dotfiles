f2e22ac99ca4fc3f6d16a4ee602b29ce4ab9b028
Ellie Johnston <jellie7118@proton.me>
2026-02-01 15:50:58 +0000

    Fix misspelling

--- hosts/x86_64-linux-laptop/lenowo/configuration.nix
+++ hosts/x86_64-linux-laptop/lenowo/configuration.nix
@@ -29,7 +29,7 @@
 
   system.stateVersion = "25.11";
 
-  networking.networkManager.dns = "none";
+  networking.networkmanager.dns = "none";
   networking.nameservers = [
     "1.1.1.1"
     "8.8.8.8"
